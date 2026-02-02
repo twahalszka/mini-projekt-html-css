5ad7e15ab9d1c3c16dad43eceed9ebc51d77f7dd
twahalszka <twahalszka@gmail.com>
2026-02-02 10:39:42 +0100

    Ćwiczenie w JavaScript
    Ćwiczenie dodawania i zmiany przycisku na stronie za pomocą JavaScript

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ const button = document.getElementById('changeTextBtn');
 const text = document.getElementById('text');
 
 button.addEventListener('click', function () {
-  text.textContent = 'Tekst został zmieniony!';
+  text.textContent = 'Denkujeee';
+});
+
+button.addEventListener('click', function () {
+  text.textContent = 'Bardzo hihi';
 });
